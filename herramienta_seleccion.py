ac2c9689d06c4c37bed032a405f43ddf18cf06b5
juan jose llano <jjllanoa>
2019-04-24 14:23:21 -0500

    Se inserta la impedancia de los conductores y se inicia la construcción de las función de regulación de voltaje

--- herramienta_seleccion.py
+++ herramienta_seleccion.py
@@ -11,12 +11,11 @@ Created on Mon Apr 22 15:49:38 2019
 from tkinter import *
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import back_functions
 
 
 root=Tk()
 root.title('Herramienta selección óptima de conductores')
-principal_frame=Frame(root)
-principal_frame.pack()
 #--------------------------------------------------------------
 #Variables creation
 load=StringVar() #Circuit load
@@ -36,37 +35,61 @@ lifetime.set(20)
 phases=IntVar() #Phases number of the circuit
 phases.set(1)
 
-kwh_cost=IntVar() #Electricty cost in COP
+kwh_cost=IntVar() #Electricity cost in COP
 kwh_cost.set(550)
+
+conduit_materials=('PVC', 'Aluminio', 'Acero')
+actual_conduit_material=StringVar()
+actual_conduit_material.set('PVC')
+
+wire_materials=('Cobre','Aluminio')
+actual_wire_material=StringVar()
+actual_wire_material.set('Cobre')
+
+
 #--------------------------------------------------------------
 #Input data creation 
-Label(principal_frame, text='Carga[KW+JKVAR]').grid(row=0,column=0)
-Entry(principal_frame, textvariable=load, width="15").grid(row=1, column=0,padx=5)
 
-Label(principal_frame, text='Longitud[m]').grid(row=0,column=1)
-Entry(principal_frame, textvariable=length, width="10").grid(row=1, column=1,padx=5)
+data_frame=Frame(root)
+data_frame.pack()
 
-Label(principal_frame, text='Voltaje[V]').grid(row=0,column=2)
-Entry(principal_frame, textvariable=voltage, width="8").grid(row=1, column=2, padx=5)
+Label(data_frame, text='Carga[KW+JKVAR]').grid(row=0,column=0)
+Entry(data_frame, textvariable=load, width="15").grid(row=1, column=0,padx=5)
 
-Label(principal_frame, text='Tasa energía[%/Año]').grid(row=0,column=3)
-Entry(principal_frame, textvariable=rate, width="16").grid(row=1, column=3)
+Label(data_frame, text='Longitud[m]').grid(row=0,column=1)
+Entry(data_frame, textvariable=length, width="10").grid(row=1, column=1,padx=5)
 
-Label(principal_frame, text='Vida útil[Años]').grid(row=0,column=4)
-Entry(principal_frame, textvariable=lifetime, width="12").grid(row=1, column=4, padx=5)
+Label(data_frame, text='Voltaje[V]').grid(row=0,column=2)
+Entry(data_frame, textvariable=voltage, width="8").grid(row=1, column=2, padx=5)
 
-Label(principal_frame, text='Costo kWh').grid(row=0,column=5)
-Entry(principal_frame, textvariable=kwh_cost, width="8").grid(row=1, column=5)
+Label(data_frame, text='Tasa energía[%/Año]').grid(row=0,column=3)
+Entry(data_frame, textvariable=rate, width="16").grid(row=1, column=3)
 
-Radiobutton(principal_frame, text='Circuito monofásico', variable=phases,value=1 ).grid(row=0,column=6)
-Radiobutton(principal_frame, text='Circuito trifásico', variable=phases,value=3 ).grid(row=1,column=6)
+Label(data_frame, text='Vida útil[Años]').grid(row=0,column=4)
+Entry(data_frame, textvariable=lifetime, width="12").grid(row=1, column=4, padx=5)
 
-Button(principal_frame, text='Realizar análisis').grid(row=0, column=7)
+Label(data_frame, text='Costo kWh').grid(row=0,column=5)
+Entry(data_frame, textvariable=kwh_cost, width="8").grid(row=1, column=5)
 
+Label(data_frame, text='Canaleta').grid(row=0,column=6)
+OptionMenu(data_frame,actual_conduit_material,*conduit_materials).grid(row=1,column=6,padx=5)
+
+Label(data_frame, text='Conductor').grid(row=0,column=7)
+OptionMenu(data_frame,actual_wire_material,*wire_materials).grid(row=1,column=7,padx=5)
+
+Radiobutton(data_frame, text='Circuito monofásico', variable=phases,value=1 ).grid(row=0,column=8)
+Radiobutton(data_frame, text='Circuito trifásico', variable=phases,value=3 ).grid(row=1,column=8)
+
+Button(data_frame, text='Realizar análisis').grid(row=0, column=9)
+
+#------------------------------------------------------------------
+graph_frame=Frame(root)
+graph_frame.pack()
 figure1=plt.Figure(figsize=(4,3))
 graph = figure1.add_subplot(111)
 graph.plot([1,2,3,4,5,6],[1,2,3,4,5,6])
-canvas = FigureCanvasTkAgg(figure1, principal_frame)
-canvas.get_tk_widget().grid(row=2)
+canvas = FigureCanvasTkAgg(figure1, graph_frame)
+canvas.get_tk_widget().grid(row=0, column=0)
+
 
 root.mainloop()
